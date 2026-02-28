e2d9a509e7459cae8fc86576879802a760d47570
riyaskh123 <riyaskh123@gmail.com>
2026-02-28 14:52:08 +0400

    whatsapp url

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,18 +18,17 @@ const Hero = () => {
       {/* Content */}
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white px-6">
 
-        <p className="tracking-[0.4em] text-sm text-[#C5A880] mb-6 uppercase">
+        <p className="tracking-[0.4em] text-sm text-[#C5A880] mb-6 uppercase font-semibold">
           Wedding Invitation
         </p>
 
-        <h1 className="text-4xl md:text-7xl font-heading mb-2">
-          Fathima Kareem
+        <h1 className="text-4xl md:text-7xl font-heading" style={{ fontFamily: '"Pacifico", cursive',fontWeight: 300 }}>
+          Muhammed Riyas
         </h1>
 
         <p className="text-[#C5A880] text-6xl italic my-2">&</p>
-
-        <h1 className="text-4xl md:text-7xl font-heading">
-          Muhammed Riyas
+        <h1 className="text-4xl md:text-7xl font-heading mb-2" style={{ fontFamily: '"Pacifico", cursive',fontWeight: 300 }}>
+          Fathima Kareem
         </h1>
 
         <p className="mt-4 text-gray-200 max-w-md italic">
